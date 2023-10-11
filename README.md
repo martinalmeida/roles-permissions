985f48507a4a8b9774f9b6b5ed412a4067b4eeb0
martinalmeida <martinalmeida56@gmail.com>
2023-10-11 01:45:27 -0500

    feat: change name folder in shared

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a system made in Laravel 10 for the administration and creation of users with roles and permissions to modules or pages dynamically, incorporating vue.js 3 and tailwind 3.
 
-## Clone the repository:
+### Clone the repository:
 
 Start by cloning this repository on your local machine.
 
--- resources/js/modules/home/views/HomeView.vue
+++ resources/js/modules/home/views/HomeView.vue
@@ -8,12 +8,8 @@
 </template>
 
 <script setup>
-import { useHomeStore } from "@/modules/home/store/homeStore.js";
-
-import Sidebar from "@/modules/shared/components/home/sidebar.vue";
-import Head from "@/modules/shared/components/home/Head.vue";
-import Search from "@/modules/shared/components/home/Search.vue";
-import Content from "@/modules/shared/components/home/Content.vue";
-
-const homeStore = useHomeStore();
+import Sidebar from "@/modules/shared/components/layout/sidebar.vue";
+import Head from "@/modules/shared/components/layout/Head.vue";
+import Search from "@/modules/shared/components/layout/Search.vue";
+import Content from "@/modules/shared/components/layout/Content.vue";
 </script>
