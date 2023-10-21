198daf8f1f38d62cdc61704d24a68bb860c0acdb
martinalmeida <martinalmeida56@gmail.com>
2023-10-20 19:54:57 -0500

    feat: new types in migrations

--- database/migrations/2023_10_10_061915_create_modules_table.php
+++ database/migrations/2023_10_10_061915_create_modules_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('icon');
+            $table->mediumText('icon')->nullable();
             $table->string('description');
             $table->smallInteger('status')->default(1);
             $table->timestamps();
--- resources/js/modules/shared/store/state.js
+++ resources/js/modules/shared/store/state.js
@@ -29,16 +29,25 @@ export const useState = defineStore("shared.state", {
                 nit: 0,
             },
             modules: {
+                id: 1,
                 name: "Roles y Permisos",
                 icon: "mdi-account-key",
+                description: "Roles y permisos del sistema",
+                status: 0,
                 subModules: [
                     {
                         name: "Roles",
-                        url: "roles",
+                        page: "roles",
+                        description: "Roles del sistema",
+                        status: 0,
+                        moduleId: 1
                     },
                     {
                         name: "Permisos",
-                        url: "permissions",
+                        page: "permissions",
+                        description: "Permisos del sistema",
+                        status: 0,
+                        moduleId: 1
                     },
                 ]
             },
