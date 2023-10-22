4a5a2dfaeddfbc9754948afdd8fab5da798a2934
Mfgb.05171513 <brahyan.com@gmail.com>
2023-10-21 22:55:10 -0500

    feat: add new file seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ModuleSeeder::class);
         $this->call(SubModuleSeeder::class);
+        $this->call(PermissionSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
